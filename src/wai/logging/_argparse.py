7627cf9bef4200f3bf0e80a2161b948767ac84f5
Peter Reutemann <fracpete@gmail.com>
2023-11-30 12:14:50 +1300

    help string can be customized now as well

--- src/wai/logging/_argparse.py
+++ src/wai/logging/_argparse.py
@@ -3,7 +3,8 @@ import argparse
 from ._core import LOGGING_LEVELS, LOGGING_WARN
 
 
-def add_logging_level(parser: argparse.ArgumentParser, short_opt: str = "-l", long_opt: str = "--logging_level"):
+def add_logging_level(parser: argparse.ArgumentParser, short_opt: str = "-l", long_opt: str = "--logging_level",
+                      help_str: str = None):
     """
     Adds an option for the logging level to the parser.
 
@@ -13,12 +14,16 @@ def add_logging_level(parser: argparse.ArgumentParser, short_opt: str = "-l", lo
     :type short_opt: str
     :param long_opt: the long option flag to use (eg --logging_level)
     :type long_opt: str
+    :param help_str: the help string to use
+    :type help_str: str
     """
-    parser.add_argument(short_opt, long_opt, choices=LOGGING_LEVELS, default=LOGGING_WARN,
-                        help="The logging level to use.")
+    if help_str is None:
+        help_str = "The logging level to use."
+    parser.add_argument(short_opt, long_opt, choices=LOGGING_LEVELS, default=LOGGING_WARN, help=help_str)
 
 
-def add_logger_name(parser: argparse.ArgumentParser, short_opt: str = "-N", long_opt: str = "--logger_name"):
+def add_logger_name(parser: argparse.ArgumentParser, short_opt: str = "-N", long_opt: str = "--logger_name",
+                    help_str: str = None):
     """
     Adds an option for the logger name to the parser.
 
@@ -28,6 +33,9 @@ def add_logger_name(parser: argparse.ArgumentParser, short_opt: str = "-N", long
     :type short_opt: str
     :param long_opt: the long option flag to use (eg --logger_name)
     :type long_opt: str
+    :param help_str: the help string to use
+    :type help_str: str
     """
-    parser.add_argument(short_opt, long_opt, type=str, default=None,
-                        help="The custom name to use for the logger", required=False)
+    if help_str is None:
+        help_str = "The custom name to use for the logger"
+    parser.add_argument(short_opt, long_opt, type=str, default=None, help=help_str, required=False)
